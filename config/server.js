e2d12ae2060fcdd4f1b273ab97af253a88cc5a1f
Anupam <anupam120k@gmail.com>
2026-04-15 18:39:24 +0530

    fix: enforce render port binding and remove conflicting env

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: '0.0.0.0',
-  port: env.int('PORT', 1337),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 10000),
   app: {
     keys: env.array('APP_KEYS'),
   },
